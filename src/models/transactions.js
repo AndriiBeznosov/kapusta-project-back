d292e0a17e8335af3cc269c6a12acedf4de707fe
Andrii Beznosov <andrey301288@gmail.com>
2023-02-18 17:35:11 +0200

    add transaction

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -4,7 +4,6 @@ const { Transaction } = require("../schemas/transactions");
 const addTransaction = async (data, id) => {
   try {
     data.userId = id;
-    console.log(data);
     const transaction = await Transaction.create(data);
     return transaction;
   } catch (error) {
