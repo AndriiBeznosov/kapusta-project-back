c0f061ca453270c987be0ca410a6fd7fd0ac784f
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-02-18 11:36:23 +0200

    add auth

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { register, login, logout } = require("../controllers/users.controller");
 const { tryCatchWrapper } = require("../tryCatchWrapper/tryCatchWrapper");
+const { auth } = require("../middlewares/auth");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/register", tryCatchWrapper(register));
 usersRouter.post("/login", tryCatchWrapper(login));
-usersRouter.patch("/logout", tryCatchWrapper(logout));
+usersRouter.patch("/logout", auth, tryCatchWrapper(logout));
 
 module.exports = usersRouter;
