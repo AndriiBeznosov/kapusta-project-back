b07a4bf0580b81a3cf151c867983150a3e3bedf7
Andrii Beznosov <andrey301288@gmail.com>
2023-02-21 17:50:23 +0200

    fix createConfirmationMail

--- src/helpers/createConfirmationMail.js
+++ src/helpers/createConfirmationMail.js
@@ -2,7 +2,8 @@ const createConfirmationMail = (userEmail, verificationToken) => {
   const mail = {
     to: userEmail,
     subject: 'Kapusta app. Confirm your email',
-    html: `<a target='_blank' href='http://localhost:${process.env.PORT}/api/users/verify/${verificationToken}'> <strong> Click on link to confirm your email </strong> </a>`,
+    html: `<div>
+    <a target='_blank' href=${process.env.SERVER_URL}/api/users/verify/${verificationToken}> <strong> Click on link to confirm your email </strong> </a></div>`,
   };
 
   return mail;
