db18db5bef332f5f8edf4fee09b360e59402baba
Andrii Beznosov <andrey301288@gmail.com>
2023-02-26 20:37:38 +0200

    delete google-redirect

--- src/routes/authGoogle.js
+++ src/routes/authGoogle.js
@@ -8,6 +8,6 @@ const { tryCatchWrapper } = require('../tryCatchWrapper/tryCatchWrapper');
 const authGoogleRouter = express.Router();
 
 authGoogleRouter.get('/auth/google', tryCatchWrapper(googleAuth));
-authGoogleRouter.get('/auth/google-redirect', tryCatchWrapper(googleRedirect));
+authGoogleRouter.get('/auth', tryCatchWrapper(googleRedirect));
 
 module.exports = authGoogleRouter;
