0d8d564f9243babc516bbf4e36369c3b0eb8924e
Andrii Beznosov <andrey301288@gmail.com>
2023-03-01 20:09:07 +0200

    fix tryCatchWrapper

--- src/tryCatchWrapper/tryCatchWrapper.js
+++ src/tryCatchWrapper/tryCatchWrapper.js
@@ -1,12 +1,12 @@
-// const HttpError = require('../httpError');
+const HttpError = require('../httpError');
 
 const tryCatchWrapper = endpointFn => {
   return async (req, res, next) => {
     try {
       await endpointFn(req, res, next);
     } catch (error) {
-      res.status(error.code).json({ message: error.message });
-      // throw new HttpError(error.code, error.message);
+      // res.status(error.code).json({ message: error.message });
+      throw new HttpError(error.code, error.message);
     }
   };
 };
